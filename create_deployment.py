e11428b11612e7e2508f293e90e45f7848b9218a
TelRich <okorogoodrich@gmail.com>
2024-03-26 12:30:50 +0100

    push deployment file

---
+++ create_deployment.py
@@ -0,0 +1,11 @@
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/TelRich/Prefect.git",
+        entrypoint="git_wrkflw:repo_info",
+    ).deploy(
+        name="my-first-deployment",
+        work_pool_name="my-managed-pool",
+        cron="0 1 * * *",
+    )
