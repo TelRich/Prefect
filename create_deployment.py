4fd8408bf91c5e8428fe03ca98af553f22219628
TelRich <okorogoodrich@gmail.com>
2024-10-31 12:23:04 +0100

    new changes

--- create_deployment.py
+++ create_deployment.py
@@ -1,15 +1,5 @@
 from prefect import flow
 
-# if __name__ == "__main__":
-#     flow.from_source(
-#         source="https://github.com/TelRich/Prefect.git",
-#         entrypoint="git_wrkflw.py:repo_info",
-#     ).deploy(
-#         name="my-first-deployment",
-#         work_pool_name="my-managed-pool",
-#         cron="0 1 * * *",
-#     )
-
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/TelRich/Prefect.git",
--- mv.py
+++ mv.py
@@ -3,112 +3,58 @@ from prefect import get_run_logger
 import psycopg2
 from psycopg2 import OperationalError
 import time
-
 from prefect.blocks.system import Secret
 
-secret_block = Secret.load("db-pw")
-
-# Access the stored secret
-pw = secret_block.get()
-
-
-secret_block1 = Secret.load("host")
-
-# Access the stored secret
-host = secret_block1.get()
-
+# Load secrets
+pw = Secret.load("db-pw").get()
+host = Secret.load("host").get()
 
 @task
 def connect():
-    """Connect to the PostgreSQL database server\n"""
+    """Connect to the PostgreSQL database server"""
     try:
-        start_time = time.time()
         logger = get_run_logger()
-        logger.info("""Connecting to the database.......\n""")
+        logger.info("Connecting to the database...")
         db_config = {
             "dbname": "warehouse",
             "user": "goodrich_okoro",
-            "password": f"{pw}",
-            "host": f"{host}"
-        }   
-        
-        # Open a cursor to perform database operations
+            "password": pw,
+            "host": host
+        }
         conn = psycopg2.connect(**db_config)
         cur = conn.cursor()
-        end_time = time.time()
-        
-        duration = round((end_time - start_time)/60.0,2)
-        logger.info("Connection Suceessful 👍\n")
-        logger.info(f"Execution time: {duration} minutes\n")
+        logger.info("Connection Successful 👍")
         return conn, cur
-    
     except OperationalError as e:
-        logger.info(f"Error: {e}")
-        
-        
+        logger = get_run_logger()
+        logger.error(f"Error: {e}")
+        return None, None
 
+@task
+def close_connection(conn, cur):
+    if conn and cur:
+        cur.close()
+        conn.close()
 
 @flow
 def refresh_bfree():
     logger = get_run_logger()
-    logger.info('\nRefreshing BFREE Materialized View..... Please hold on.....\n')
+    logger.info('Refreshing BFREE Materialized View... Please hold on...')
     start_time = time.time()
-    conn,cur = connect()
-    query = """
-    REFRESH MATERIALIZED VIEW bi.bfree
-    """
-    logger.info('Refreshing view......\n')
-    cur.execute(query)
-    conn.commit()
-    end_time = time.time()
-    duration = round((end_time - start_time)/60.0,2)
     
-    logger.info("Done refreshing the view \n")
-    logger.info(f"Execution time: {duration} minutes\n")
+    conn, cur = connect()
+    if conn and cur:
+        try:
+            query = "REFRESH MATERIALIZED VIEW bi.bfree"
+            logger.info('Refreshing view...')
+            cur.execute(query)
+            conn.commit()
+            logger.info("Done refreshing the view.")
+        finally:
+            close_connection(conn, cur)
     
-# @flow
-# def refresh_originations():
-#     logger = get_run_logger()
-#     logger.info('\nRefreshing Originations Materialized View..... Please hold on.....\n')
-#     start_time = time.time()
-#     conn,cur = connect()
-#     query = """
-#     REFRESH MATERIALIZED VIEW bi.originations
-#     """
-#     logger.info('Refreshing view......\n')
-#     cur.execute(query)
-#     conn.commit()
-#     end_time = time.time()
-#     duration = round((end_time - start_time)/60.0,2)
-    
-#     logger.info("Done refreshing the view \n")
-#     logger.info(f"Execution time: {duration} minutes\n")
-    
+    duration = round((time.time() - start_time) / 60.0, 2)
+    logger.info(f"Execution time: {duration} minutes")
+
 if __name__ == "__main__":
     refresh_bfree()
-    
-    # refresh_bfree.serve(
-    #     name="bfree-deployment",
-    #     cron="5 * * * *",
-    #     work_pool_name="my-managed-pool",
-    #     tags=["mv", "bfree"],
-    #     description="refresh bfree materialized view",
-    #     version="mv/deployments",
-    # )
-    
-    # refresh_originations()
-    
-# if __name__ == "__main__":
-#     get_repo_info.serve(
-#         name="my-first-deployment",
-#         cron="* * * * *",
-#         tags=["testing", "tutorial"],
-#         description="Given a GitHub repository, logs repository statistics for that repo.",
-#         version="tutorial/deployments",
-#     )
-    
-
-    
-
-
-    
